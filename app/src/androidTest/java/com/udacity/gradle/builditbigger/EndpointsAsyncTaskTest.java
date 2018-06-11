aeb2b0d0bfca05f4d408f4b04038f1707d2ca4c6
LaviniaDragunoi <lavinia.dragunoi@yahoo.ro>
2018-06-12 00:53:20 +0300

    error on AsyncTask Test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,6 +1,17 @@
 package com.udacity.gradle.builditbigger;
-
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static android.support.test.espresso.Espresso.onView;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
